706c2b6252a93cdd50a9369374c84e0c36f483b4
Kevin Cooper <cooperka92@gmail.com>
2020-01-09 19:28:37 -0800

    Allow full piano range (A0 - C8)

--- src/lib/constants/NOTES.js
+++ src/lib/constants/NOTES.js
@@ -1,5 +1,5 @@
 const TONES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
-const OCTAVE_NUMBERS = [1, 2, 3, 4, 5, 6, 7];
+const OCTAVE_NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 export default OCTAVE_NUMBERS.reduce((notes, octaveNumber) => {
   const notesInOctave = TONES.map(tone => `${tone}${octaveNumber}`);
